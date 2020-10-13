1fe4cd7af5e69a9c5be4ea230cd361c6dd68cc79
Thura-htet <lxgsts@gmail.com>
2020-10-13 12:05:07 +0630

    added home_page page and show_post and post_list_view API end points with some url improvements

---
+++ posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+urlpatterns = [
+    path('', views.home_page, name='home_page'),
+    path('post/<int:post_id>', views.show_post, name='show_post'),
+    path('posts/', views.post_list_view, name='all_posts')
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
 
+from .models import Post
 # Create your views here.
+
+def home_page(request, *args, **kwargs):
+    return render(request, 'pages/homepage.html', {}, status=200)
+
+def post_list_view(request, *args, **kwargs):
+    try:
+        status = 200
+        qs = Post.objects.all()
+        posts_list = [
+            {
+                'id': x.id,
+                'title': x.title,
+                'summary': x.summary,
+                'content': x.content
+            } for x in qs]
+        data = {
+            'response': posts_list,
+            'message': 'success'
+        }
+    except:
+        data['message'] = 'error: page not found'
+        status = 400
+    return JsonResponse(data, status=status)
+
+def show_post(request, post_id, *args, **kwargs):
+    try:
+        post = Post.objects.get(id=post_id)
+        data = {
+            'response': {
+                'id': post.id,
+                'title': post.title,
+                'summary': post.summary,
+                'content': post.content
+            },
+            'message': 'success'
+        }
+        status = 200
+    except:
+        data['message'] = 'error: page not found'
+        status = 404
+    return JsonResponse(data, status=status)
+    
