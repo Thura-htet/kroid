28912ab1a6527d07d80f8434b9e3e0b755dedee2
Thura-htet <lxgsts@gmail.com>
2020-10-19 17:36:48 +0630

    added url for comment view

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('write/', views.write_page, name='write'),
     path('post/<int:post_id>', views.post_detail, name='post_detial'),
     path('post/action', views.post_action, name='post_action'),
-    path('posts/', views.post_list_view, name='all_posts')
+    path('posts/', views.post_list_view, name='all_posts'),
+    path('post/<int:post_id>/comments', views.comment, name='comments')
 ]
