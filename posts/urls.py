b1dd8fd664c1124d25d879fdcb4a2ed35fd6e76b
Thura-htet <lxgsts@gmail.com>
2020-10-22 09:46:16 +0630

    .gitignore and urls.py changes

--- posts/urls.py
+++ posts/urls.py
@@ -4,8 +4,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
-    path('write/', views.write_page, name='write'),
     path('posts/', views.post_list_view, name='all_posts'),
     path('post/<int:post_id>/', views.post_detail, name='post_detial'),
     path('post/<int:post_id>/comments/', views.comment, name='comments')
