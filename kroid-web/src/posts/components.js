3054ae09d26f3fb0214c4f81207382280c62f89a
Thura-htet <lxgsts@gmail.com>
2020-10-24 13:59:57 +0630

    style changes

--- kroid-web/src/posts/components.js
+++ kroid-web/src/posts/components.js
@@ -41,20 +41,36 @@ export function PostList(props)
 export function ActionButton(props)
 {
   const {post, action} = props
-  return (
-    action.type === 'save' ? <button>Save</button> : null
-  )
+  if (action.type === 'save')
+  {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bookmark-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5V2z"/>
+      </svg>
+    )
+  }
+  else
+  {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bookmark-check-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M4 0a2 2 0 0 0-2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4zm6.854 5.854a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+      </svg>
+    )
+  }
 }
 
 export function Post(props)
 {
   const { post } = props;
-  const className = props.className ? props.className : 'col-10 mx-auto column-md-6';
+  const className = props.className ? props.className : 'media';
   return (
     <div className={className}>
-      <p>{post.id} - { post.title }</p>
+      <div className='media-body'>
+        <h4 className='mt-0'><a href={post.url}>{ post.title}</a></h4>
+          <p>{post.summary}</p>
+      </div>
       <div>
-        <ActionButton post={post} action={{type: 'save'}} />
+          <ActionButton post={post} action={{type: 'save'}} />
       </div>
     </div>
   )
